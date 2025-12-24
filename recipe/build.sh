705579423d3b84e7f6f0a3401e2712130ad0d5f8
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-24 14:19:56 -0800

    Add isis feedstock

---
+++ recipe/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+export ISISROOT=$(pwd) # very important
+export PREFIX=$CONDA_PREFIX
+echo PREFIX=$PREFIX
+cmake                                                 \
+    ../isis                                           \
+    -GNinja                                           \
+    -DJP2KFLAG=OFF                                    \
+    -Dpybindings=OFF                                  \
+    -DbuildDocs=OFF                                   \
+    -DbuildTests=OFF                                  \
+    -DCMAKE_BUILD_TYPE=Release                        \
+    -DNN_INCLUDE_DIR=$CONDA_PREFIX/include            \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                  \
+    -DOPENCV_INCLUDE_DIR:PATH=$PREFIX/include/opencv4 \
+    -DCMAKE_CXX_FLAGS="-I$CONDA_PREFIX/include"
+ninja install
+

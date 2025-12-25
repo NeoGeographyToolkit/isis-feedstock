28a3efd0445ce35766fdc6439b2e36465b3d1ee1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-24 17:51:21 -0800

    Tune recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,7 @@ cmake                                                 \
     -DNN_INCLUDE_DIR=$CONDA_PREFIX/include            \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                  \
     -DOPENCV_INCLUDE_DIR:PATH=$PREFIX/include/opencv4 \
-    -DCMAKE_CXX_FLAGS="-I$CONDA_PREFIX/include -Wno-error"
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=15.0                \
+    -DCMAKE_CXX_FLAGS="-I$CONDA_PREFIX/include -Wno-error -Wno-deprecated-builtins"
 ninja install
 
